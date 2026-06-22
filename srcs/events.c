6017a993ef74d80336048132ff1d6b7dee125d31
makui <makui@f0r10s4.paris.42.school>
2026-06-22 22:04:16 +0200

    manage collision

--- srcs/events.c
+++ srcs/events.c
@@ -32,55 +32,87 @@ int	handle_keypress(int keycode, void *param)
 	double	move_speed;
 	double	player_safety_radius;
 
+	double	next_x;
+	double	next_y;
+	double	check_x;
+	double	check_y;
+
 	double	old_dir_x;
 	double	old_plane_x;
 	double	rot_speed;
 
 	game = (t_game *)param;
-	move_speed = 0.1;
-	player_safety_radius = ((double)((TILE_SIZE / 3) / 2) / TILE_SIZE) + 0.01;
 	if (keycode == 65307)
 		close_game(game);
+	player_safety_radius = ((double)((TILE_SIZE / 3) / 2) / TILE_SIZE) + 0.01;
+	move_speed = 0.1;
 	if (keycode == 119)
 	{
-		if (game->map[(int)(game->player_y - move_speed - player_safety_radius)][(int)game->player_x] != '1' &&
-			game->map[(int)(game->player_y - move_speed - player_safety_radius)][(int)(game->player_x + player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y - move_speed - player_safety_radius)][(int)(game->player_x - player_safety_radius)] != '1')
-		{
-			game->player_x += game->dir_x * move_speed;
-			game->player_y += game->dir_y * move_speed;
-		}
+		next_x = game->player_x + game->dir_x * move_speed;
+		next_y = game->player_y + game->dir_y * move_speed;
+		if (next_x >= game->player_x)
+			check_x = next_x + player_safety_radius;
+		else
+			check_x = next_x - player_safety_radius;
+		if (game->map[(int)game->player_y][(int)check_x] != '1')
+			game->player_x = next_x;
+		if (next_y >= game->player_y)
+			check_y = next_y + player_safety_radius;
+		else
+			check_y = next_y - player_safety_radius;
+		if (game->map[(int)check_y][(int)game->player_x] != '1')
+			game->player_y = next_y;
 	}
 	if (keycode == 115)
 	{
-		if (game->map[(int)(game->player_y + move_speed + player_safety_radius)][(int)game->player_x] != '1' &&
-			game->map[(int)(game->player_y + move_speed + player_safety_radius)][(int)(game->player_x + player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y + move_speed + player_safety_radius)][(int)(game->player_x - player_safety_radius)] != '1')
-		{
-			game->player_x -= game->dir_x * move_speed;
-			game->player_y -= game->dir_y * move_speed;
-		}
+		next_x = game->player_x - game->dir_x * move_speed;
+		next_y = game->player_y - game->dir_y * move_speed;
+		if (next_x >= game->player_x)
+			check_x = next_x + player_safety_radius;
+		else
+			check_x = next_x - player_safety_radius;
+		if (game->map[(int)game->player_y][(int)check_x] != '1')
+			game->player_x = next_x;
+		if (next_y >= game->player_y)
+			check_y = next_y + player_safety_radius;
+		else
+			check_y = next_y - player_safety_radius;
+		if (game->map[(int)check_y][(int)game->player_x] != '1')
+			game->player_y = next_y;
 	}
 	if (keycode == 97)
 	{
-		if (game->map[(int)game->player_y][(int)(game->player_x - move_speed - player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y + player_safety_radius)][(int)(game->player_x - move_speed - player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y - player_safety_radius)][(int)(game->player_x - move_speed - player_safety_radius)] != '1')
-		{
-			game->player_x += game->dir_y * move_speed;
-			game->player_y -= game->dir_x * move_speed;
-		}
-
+		next_x = game->player_x + game->dir_y * move_speed;
+		next_y = game->player_y - game->dir_x * move_speed;
+		if (next_x >= game->player_x)
+			check_x = next_x + player_safety_radius;
+		else
+			check_x = next_x - player_safety_radius;
+		if (game->map[(int)game->player_y][(int)check_x] != '1')
+			game->player_x = next_x;
+		if (next_y >= game->player_y)
+			check_y = next_y + player_safety_radius;
+		else
+			check_y = next_y - player_safety_radius;
+		if (game->map[(int)check_y][(int)game->player_x] != '1')
+			game->player_y = next_y;
 	}
 	if (keycode == 100)
 	{
-		if (game->map[(int)game->player_y][(int)(game->player_x + move_speed + player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y + player_safety_radius)][(int)(game->player_x + move_speed + player_safety_radius)] != '1' &&
-			game->map[(int)(game->player_y - player_safety_radius)][(int)(game->player_x + move_speed + player_safety_radius)] != '1')
-		{
-			game->player_x -= game->dir_y * move_speed;
-			game->player_y += game->dir_x * move_speed;
-		}
+		next_x = game->player_x - game->dir_y * move_speed;
+		next_y = game->player_y + game->dir_x * move_speed;
+		if (next_x >= game->player_x)
+			check_x = next_x + player_safety_radius;
+		else
+			check_x = next_x - player_safety_radius;
+		if (game->map[(int)game->player_y][(int)check_x] != '1')
+			game->player_x = next_x;
+		if (next_y >= game->player_y)
+			check_y = next_y + player_safety_radius;
+		else
+			check_y = next_y - player_safety_radius;
+		if (game->map[(int)check_y][(int)game->player_x] != '1')
+			game->player_y = next_y;
 	}
 	rot_speed = 0.05;
 	if (keycode == 65363)
@@ -100,6 +132,6 @@ int	handle_keypress(int keycode, void *param)
 		old_plane_x = game->plane_x;
 		game->plane_x = game->plane_x * cos(-rot_speed) - game->plane_y * sin(-rot_speed);
 		game->plane_y = old_plane_x * sin(-rot_speed) + game->plane_y * cos(-rot_speed);
-	} 
+	}
 	return (0);
 }
